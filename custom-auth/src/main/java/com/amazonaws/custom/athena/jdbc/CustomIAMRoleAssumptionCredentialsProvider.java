4f53eaa10c86f14947343e594c57223ca02f81c6
Kostya <kostya@perxtech.com>
2021-05-26 22:09:25 +0800

    formatting

--- custom-auth/src/main/java/com/amazonaws/custom/athena/jdbc/CustomIAMRoleAssumptionCredentialsProvider.java
+++ custom-auth/src/main/java/com/amazonaws/custom/athena/jdbc/CustomIAMRoleAssumptionCredentialsProvider.java
@@ -9,7 +9,7 @@ import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder
 import com.amazonaws.services.securitytoken.model.AssumeRoleRequest;
 import com.amazonaws.services.securitytoken.model.AssumeRoleResult;
 
-public class CustomIAMRoleAssumptionCredentialsProvider implements com.amazonaws.auth.AWSCredentialsProvider{
+public class CustomIAMRoleAssumptionCredentialsProvider implements com.amazonaws.auth.AWSCredentialsProvider {
 
 	private final AWSCredentials credentials;
 	private final String roleArn;
@@ -17,35 +17,32 @@ public class CustomIAMRoleAssumptionCredentialsProvider implements com.amazonaws
 	private AWSSecurityTokenService stsClient;
 
 	//To use in JDBC: set aws_credentials_provider_class = "com.amazonaws.custom.athena.jdbc.CustomIAMRoleAssumptionCredentialsProvider"
-    // set AwsCredentialsProviderArguments = "<accessID>,<secretKey>,<roleArn>,<region>"
-	public CustomIAMRoleAssumptionCredentialsProvider(String accessId, String secretKey, String roleArn, String region){
-
+  // set AwsCredentialsProviderArguments = "<accessID>,<secretKey>,<roleArn>,<region>"
+	public CustomIAMRoleAssumptionCredentialsProvider(String accessId, String secretKey, String roleArn, String region) {
 		this.credentials = new BasicAWSCredentials(accessId,secretKey);
 		this.roleArn = roleArn;
 
-		stsClient = AWSSecurityTokenServiceClientBuilder.standard().withCredentials((com.amazonaws.auth.AWSCredentialsProvider) new AWSStaticCredentialsProvider(credentials)).withRegion(region).build();
+		stsClient = AWSSecurityTokenServiceClientBuilder.standard()
+				.withCredentials((com.amazonaws.auth.AWSCredentialsProvider) new AWSStaticCredentialsProvider(credentials))
+				.withRegion(region)
+				.build();
 
 		refresh();
-
 	}
 
-	public AWSCredentials getCredentials() {
+  public void refresh() {
+    AssumeRoleResult result = stsClient.assumeRole(new AssumeRoleRequest().withRoleArn(roleArn).withRoleSessionName("athenajdbc"));
+    assumedCredentials = getCredentialsFromAssumedRoleResult(result);
+  }
 
+	public AWSCredentials getCredentials() {
 		return assumedCredentials;
 	}
 
-	public void refresh() {
-
-		AssumeRoleResult result = stsClient.assumeRole(new AssumeRoleRequest().withRoleArn(roleArn).withRoleSessionName("athenajdbc"));
-		assumedCredentials = getCredentialsFromAssumedRoleResult(result);
-
-	}
-
-	protected AWSCredentials getCredentialsFromAssumedRoleResult(AssumeRoleResult result){
-
-		return new BasicSessionCredentials(result.getCredentials().getAccessKeyId(),
+	protected AWSCredentials getCredentialsFromAssumedRoleResult(AssumeRoleResult result) {
+		return new BasicSessionCredentials(
+				result.getCredentials().getAccessKeyId(),
 				result.getCredentials().getSecretAccessKey(),
 				result.getCredentials().getSessionToken());
 	}
-
 }
